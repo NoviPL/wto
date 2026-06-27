bac3097f02a33f19fa41550c74e1e93d9e28320e
NoviPL <jaroslawnowinkowski@gmail.com>
2026-06-27 19:32:02 +0200

    Add years and tasks sync manager methods

--- lib/sync/sync_manager.dart
+++ lib/sync/sync_manager.dart
@@ -1,12 +1,24 @@
 import 'sync_users.dart';
+import 'sync_years.dart';
+import 'sync_tasks.dart';
 
 class SyncManager {
   static Future<void> syncAll() async {
     await syncUsersFromServer();
+    await syncYearsFromServer();
+    await syncTasksFromServer();
   }
 
   static Future<void> syncUsersFromServer() async {
     await SyncUsers.syncFromServer();
+  }
+
+  static Future<void> syncYearsFromServer() async {
+    await SyncYears.syncFromServer();
+  }
+
+  static Future<void> syncTasksFromServer() async {
+    await SyncTasks.syncFromServer();
   }
 
   static Future<bool> sendUser({
@@ -25,5 +37,31 @@ class SyncManager {
 
   static Future<bool> deleteUser(String id) async {
     return SyncUsers.deleteUser(id);
+  }
+
+  static Future<bool> sendYear({
+    required int year,
+  }) async {
+    return SyncYears.sendYear(year: year);
+  }
+
+  static Future<bool> sendTask({
+    required int year,
+    required String number,
+  }) async {
+    return SyncTasks.sendTask(
+      year: year,
+      number: number,
+    );
+  }
+
+  static Future<bool> deleteTask({
+    required int year,
+    required String number,
+  }) async {
+    return SyncTasks.deleteTask(
+      year: year,
+      number: number,
+    );
   }
 }
